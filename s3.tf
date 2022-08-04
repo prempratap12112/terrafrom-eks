8c8fc43ae54f2b55f6d3ecfdc5395ea63ded3012
ppsinghcalance <ppsingh@calance.com>
2022-08-04 06:43:36 -0700

    role updated

--- s3.tf
+++ s3.tf
@@ -7,4 +7,24 @@ resource "aws_s3_bucket" "djangobucket" {
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.djangobucket.id
   acl    = "public-read-write"
+  
+
+}
+
+resource "aws_s3_bucket_policy" "django-k8-policy" {
+  bucket = aws_s3_bucket.djangobucket.bucket
+  policy     = <<EOF
+  {
+    "Version": "2012-10-17",
+     "Statement": [
+         {
+             "Sid": "Allow All",
+             "Effect": "Allow",
+             "Principal": "*",
+             "Action": "s3:*",
+             "Resource": "arn:aws:s3:::$bucket/*"
+         }
+     ]
+}
+EOF
 }
